fc454304eb4c56fcfa7840713841c235553840a5
Tunaberkyrky <tunaberk.software@outlook.com>
2024-03-27 15:39:06 +0300

    Add - Categories Controller

--- ITOFLIX/Controllers/CategoriesController.cs
+++ ITOFLIX/Controllers/CategoriesController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ITOFLIX.Data;
 using ITOFLIX.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ITOFLIX.Controllers
 {
@@ -23,24 +24,16 @@ namespace ITOFLIX.Controllers
 
         // GET: api/Categories
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Category>>> GetCategory()
+        public ActionResult<List<Category>> GetCategories()
         {
-          if (_context.Categories == null)
-          {
-              return NotFound();
-          }
-            return await _context.Categories.ToListAsync();
+            return  _context.Categories.AsNoTracking().ToList();
         }
 
         // GET: api/Categories/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Category>> GetCategory(short id)
+        public ActionResult<Category> GetCategory(short id)
         {
-          if (_context.Categories == null)
-          {
-              return NotFound();
-          }
-            var category = await _context.Categories.FindAsync(id);
+            Category? category = _context.Categories.Find(id);
 
             if (category == null)
             {
@@ -53,29 +46,17 @@ namespace ITOFLIX.Controllers
         // PUT: api/Categories/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutCategory(short id, Category category)
+        [Authorize(Roles = "ContentAdmin")]
+        public ActionResult PutCategory(Category category)
         {
-            if (id != category.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(category).State = EntityState.Modified;
+            _context.Categories.Update(category);
 
             try
             {
-                await _context.SaveChangesAsync();
+                 _context.SaveChanges();
             }
-            catch (DbUpdateConcurrencyException)
+            catch (Exception ex)
             {
-                if (!CategoryExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
             }
 
             return NoContent();
@@ -84,41 +65,13 @@ namespace ITOFLIX.Controllers
         // POST: api/Categories
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Category>> PostCategory(Category category)
+        [Authorize(Roles = "ContentAdmin")]
+        public  ActionResult<short> PostCategory(Category category)
         {
-          if (_context.Categories == null)
-          {
-              return Problem("Entity set 'ITOFLIXContext.Category'  is null.");
-          }
             _context.Categories.Add(category);
-            await _context.SaveChangesAsync();
+            _context.SaveChanges();
 
-            return CreatedAtAction("GetCategory", new { id = category.Id }, category);
-        }
-
-        // DELETE: api/Categories/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteCategory(short id)
-        {
-            if (_context.Categories == null)
-            {
-                return NotFound();
-            }
-            var category = await _context.Categories.FindAsync(id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            _context.Categories.Remove(category);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool CategoryExists(short id)
-        {
-            return (_context.Categories?.Any(e => e.Id == id)).GetValueOrDefault();
+            return Ok(category.Id);
         }
     }
 }
