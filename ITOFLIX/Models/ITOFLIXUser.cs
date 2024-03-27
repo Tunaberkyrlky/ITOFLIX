485366d5b011c02d7ce8320f082d9557eee4c86f
Tunaberkyrky <tunaberk.software@outlook.com>
2024-03-27 12:08:30 +0300

    Add - User related Tables

--- ITOFLIX/Models/ITOFLIXUser.cs
+++ ITOFLIX/Models/ITOFLIXUser.cs
@@ -1,11 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
 namespace ITOFLIX.Models
 {
 	public class ITOFLIXUser : IdentityUser<long>
 	{
-		
+        [StringLength(100, MinimumLength = 2)]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Name { get; set; } = "";
+
+		[Column(TypeName= "date")]
+		public DateTime BirthDate { get; set; }
+
 	}
 }
 
---
+++ ITOFLIX/Models/UserFavorite.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ITOFLIX.Models
+{
+	public class UserFavorite
+	{
+        public long UserId { get; set; }
+        [ForeignKey("UserId")]
+        public ITOFLIXUser? ITOFLIXUser { get; set; }
+
+        public int MediaId { get; set; }
+        [ForeignKey("MediaId")]
+        public Media? Media { get; set; }
+    }
+}
+
