052f2d3eba1ea2758a6959a1b0d3cf1444078812
Tunaberkyrky <tunaberk.software@outlook.com>
2024-04-01 15:48:03 +0300

    Add - DataInitialization methods

--- ITOFLIX/Data/DataInitialization.cs
+++ ITOFLIX/Data/DataInitialization.cs
@@ -1,8 +1,7 @@
 ﻿
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNet.Identity;
 using ITOFLIX.Models;
-
+using Microsoft.EntityFrameworkCore;
 
 namespace ITOFLIX.Data
 {
@@ -11,11 +10,27 @@ namespace ITOFLIX.Data
         private readonly ITOFLIXContext _context;
         private readonly SignInManager<ITOFLIXUser> _signInManager;
         private readonly RoleManager<ITOFLIXRole> _roleManager;
-        public DataInitialization(ITOFLIXContext context, SignInManager<ITOFLIXUser> signInManager, Microsoft.AspNet.Identity.RoleManager<ITOFLIXRole> roleManager)
+        public DataInitialization(ITOFLIXContext context, SignInManager<ITOFLIXUser> signInManager, RoleManager<ITOFLIXRole> roleManager)
         {
             _context = context;
             _signInManager = signInManager;
             _roleManager = roleManager;
+            if (_context != null)
+            {
+                _context.Database.Migrate();
+                if(_context.Users.Count()==0)
+                {
+                    CreateAdminUser();
+                }
+                if(_context.Roles.Count()==0)
+                {
+                    CreateRoles();
+                }
+                if(_context.Plans.Count()==0)
+                {
+                    CreatePlans();
+                }
+            }
         }
 
         public void CreateAdminUser()
@@ -36,7 +51,26 @@ namespace ITOFLIX.Data
 
         public void CreateRoles()
         {
+            ITOFLIXRole iTOFLIXRole;
+            iTOFLIXRole = new ITOFLIXRole() { Name = "Administrator"};
+            _roleManager.CreateAsync(iTOFLIXRole).Wait();
+        }
 
+        public void CreateRestrictions()
+        {
+            _context.Restrictions.Add(new Restriction() { Id = 0, Name = "Genel İzleyici" });
+            _context.Restrictions.Add(new Restriction() { Id = 7, Name = "7 yaş ve üzeri" });
+            _context.Restrictions.Add(new Restriction() { Id = 13, Name = "13 yaş ve üzeri" });
+            _context.Restrictions.Add(new Restriction() { Id = 18, Name = "18 yaş ve üzeri" });
+
+            _context.SaveChanges();
+        }
+
+        public void CreatePlans()
+        {
+            _context.Plans.Add(new Plan() { Name = "Standart", Price = 100, Resolution = "1080" });
+
+            _context.SaveChanges();
         }
     }
 }
